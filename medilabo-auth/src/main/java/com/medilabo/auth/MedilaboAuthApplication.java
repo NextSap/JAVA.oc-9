9daf0c2ab6fe766355b82b9ce0612c99eb5918d7
NextSap <nextsap@outlook.com>
2023-08-17 11:42:29 +0200

    create auth microservice

---
+++ medilabo-auth/src/main/java/com/medilabo/auth/MedilaboAuthApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedilaboAuthApplication.class, args);
+    }
+
+}
